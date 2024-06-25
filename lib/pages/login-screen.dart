98aa0621e418a5ea7f5ae7fe0bd5e2ac84b3c1d8
gualterapple <gualter_apple@hotmail.com>
2024-06-24 23:00:20 -0400

    Teclado sobrepondo o campo de texto feito

--- lib/pages/login-screen.dart
+++ lib/pages/login-screen.dart
@@ -24,117 +24,120 @@ class _LoginScreenState extends State<LoginScreen> {
           right: TSizes.defaultSpace,
           left: TSizes.defaultSpace,
         ),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Center(
-                  child: Image(
-                      height: 100, image: AssetImage(TImagens.screenLogo)),
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: Text(
-                    'COMECE AGORA!',
-                    style: TextStyle(
-                        fontSize: TTextSizes.bigTitle,
-                        fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Center(
+                    child: Image(
+                        height: 100, image: AssetImage(TImagens.screenLogo)),
                   ),
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: Text(
-                    'Aceda a sua conta de estudante usando as suas credenciais do Portal Okayula.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: TColors.grayColor,
-                      fontWeight: FontWeight.bold,
+                  SizedBox(height: 20),
+                  Center(
+                    child: Text(
+                      'COMECE AGORA!',
+                      style: TextStyle(
+                          fontSize: TTextSizes.bigTitle,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Form(
-                  child: Column(children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Iconsax.direct_right),
-                        labelText: 'E-mail',
+                  SizedBox(height: 20),
+                  Center(
+                    child: Text(
+                      'Aceda a sua conta de estudante usando as suas credenciais do Portal Okayula.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: TColors.grayColor,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 16),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Iconsax.password_check),
-                          labelText: 'Senha',
-                          suffixIcon: Icon(Iconsax.eye_slash)),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text(
-                          'Esqueceu a sua senha ? ',
-                          style: TextStyle(color: TColors.grayColor),
+                  ),
+                  SizedBox(height: 20),
+                  Form(
+                    child: Column(children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Iconsax.direct_right),
+                          labelText: 'E-mail',
                         ),
-                        Text(
-                          'clique aqui.',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    )
-                  ]),
-                )
-              ],
-            ),
-            // Botão Continuar
-            Padding(
-              padding: const EdgeInsets.only(
-                  bottom: 20), // Ajuste o valor conforme necessário
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: Container(
-  decoration: BoxDecoration(
-    gradient: TColors.linearGradient,
-    borderRadius: BorderRadius.circular(5.0), // Borda arredondada igual ao botão
-  ),
-  child: ClipRRect(
-    borderRadius: BorderRadius.circular(5.0),
-    child: ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5.0),
-          side: BorderSide.none, // Remove a borda
-  
-        ),
-        backgroundColor: Colors.transparent, // Fundo transparente para mostrar o gradiente
-        elevation: 0,
-        padding: EdgeInsets.symmetric(vertical: 12),
-        textStyle: TextStyle(
-          fontSize: TTextSizes.bigTitle,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      onPressed: () {
-        // Ação do botão "Continuar"
-      },
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'Entrar',
-            style: TextStyle(fontSize: 18),
-          ),
-        ],
-      ),
-    ),
-  ),
-)
-
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Iconsax.password_check),
+                            labelText: 'Senha',
+                            suffixIcon: Icon(Iconsax.eye_slash)),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            'Esqueceu a sua senha ? ',
+                            style: TextStyle(color: TColors.grayColor),
+                          ),
+                          Text(
+                            'clique aqui.',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      )
+                    ]),
+                  )
+                ],
               ),
+              // Botão Continuar
+              Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 20), // Ajuste o valor conforme necessário
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Container(
+            decoration: BoxDecoration(
+              gradient: TColors.linearGradient,
+              borderRadius: BorderRadius.circular(5.0), // Borda arredondada igual ao botão
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            side: BorderSide.none, // Remove a borda
+            
+          ),
+          backgroundColor: Colors.transparent, // Fundo transparente para mostrar o gradiente
+          elevation: 0,
+          padding: EdgeInsets.symmetric(vertical: 12),
+          textStyle: TextStyle(
+            fontSize: TTextSizes.bigTitle,
+            fontWeight: FontWeight.bold,
+          ),
+                ),
+                onPressed: () {
+          // Ação do botão "Continuar"
+                },
+                child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Entrar',
+              style: TextStyle(fontSize: 18),
             ),
           ],
+                ),
+              ),
+            ),
+          )
+          
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
